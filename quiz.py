95e209fb1042d2829ae20d7a6e10680a977ba048
marco-colonna <mcolonna107@gmail.com>
2021-08-22 19:45:47 -0400

    Exercise - Python basics needed for a super sleuth personality quiz

--- quiz.py
+++ quiz.py
@@ -1,1 +1,20 @@
 print("Hello, Contosoville!")
+# this is a comment that won't be interpreted as a command
+
+# use a variable named year to "remember" the value 1990
+year = 1990
+
+# print a message to see what year it is
+print(f"The year is {year}...")
+
+year = year + 36
+
+# print a message to see what year it is now
+print(f"The year is now {year}...")
+
+# if we're in 1990
+if year == 1990:
+    print("I left you a message on your answering machine!")
+# if we're in 2026
+if year == 2026:
+    print("I sent you a text message!")
