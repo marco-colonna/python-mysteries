4e43451264d15964fe6b3547a20c4da3c7c28076
marco-colonna <mcolonna107@gmail.com>
2021-08-22 18:53:24 -0400

    Exercise - Decode one letter at a time with a Caesar cipher

--- decrypt.py
+++ decrypt.py
@@ -1,16 +1,7 @@
-print("Hello, Contosoville!")
-# This is a comment that won't be interpreted as a command.
+def lasso_letter(letter, shift_amount):
+    letter_code = ord(letter.lower())
+    decoded_letter_code = letter_code + shift_amount
+    decoded_letter = chr(decoded_letter_code)
+    return decoded_letter
 
-# Associate the variable town with the value "Contosoville"
-town = "Contosoville"
-
-# Print a message about the secret location
-print("The town I am looking for is " + town)
-
-# Define a power (function) to chant a phrase
-def chant(phrase):
-    # Glue three copies together and print it as a message
-    print(phrase + phrase + phrase)
-
-# Invoke the power to chant on the phrase "Contosoville!"
-chant("Contosoville!")
+print(lasso_letter('a', 2))
