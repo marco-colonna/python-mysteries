244327a742e61947b29bbd14259e2283367de652
marco-colonna <mcolonna107@gmail.com>
2021-08-23 14:14:22 -0400

    Challenge - Reduce the number of if, elif, and else statements

--- quiz.py
+++ quiz.py
@@ -1,55 +1,38 @@
-# ask the candidate a question
-activity = input("How would you like to spend your evening?\n(A) Reading a book\n(B) Attending a party\n")
-if activity == "A":
-    print("Reading a book, nice choice!")
-elif activity == "B":
-    print("Attending a party? Sounds fun!")
-else:
-    print("You must type A or B, let's just say you like to read.")
-    activity = "A"
+# stores the quiz questions
+questions = ["How would you like to spend your evening?\n(A) Reading a book\n(B) Attending a party\n",
+            "What's your dream job?\n(A) Curator at the Smithsonian\n(B) Running a business\n",
+            "What's more important?\n(A) Money\n(B) Love\n",
+            "What's your favorite decade?\n(A) 1910s\n(B) 2010s\n",
+            "What's your favorite way to travel?\n(A) Driving\n(B) Flying\n"]
+# stores the candidate's answers to each question
+answers = []
+# stores the responses if the candidate chooses "A"
+response_A = ["Reading a book, nice choice!\n",
+            "Curator? Sounds like a lot of responsibility...\n",
+            "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n",
+            "Great decade! Save for World War 1...\n",
+            "Driving? I hope you like cars!\n"]
+# stores the responses if the candidate chooses "B"
+response_B = ["Attending a party? Sounds fun!\n",
+            "Running a business? Sounds like a big investment...\n",
+            "<3<3<3<3<3<3<3<3<3<3<3<3<3<3<3<3\n",
+            "Great decade! Save for Donald Trump...\n",
+            "Flying? I hope you like airplanes!\n"]
 
-# ask the candidate a second question
-job = input("What's your dream job?\n(A) Curator at the Smithsonian\n(B) Running a business\n")
-if job == "A":
-    print("Curator? Sounds like a lot of responsibility...")
-elif job == "B":
-    print("Running a business? Sounds like a big investment...")
-else:
-    print("You must type A or B, let's just say you want to be a curator at the Smithsonian.")
-    job = "A"
-
-# ask the candidate a third question
-value = input("What's more important?\n(A) Money\n(B) Love\n")
-if value == "A":
-    print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-elif value == "B":
-    print("<3<3<3<3<3<3<3<3<3<3<3<3<3<3<3<3")
-else:
-    print("You must type A or B, let's just say money is more important to you.")
-    value = "A"
-
-# ask the candidate a fourth question
-decade = input("What's your favorite decade?\n(A) 1910s\n(B) 2010s\n")
-if decade == "A":
-    print("Yeah! World War 1 was great!")
-elif decade == "B":
-    print("Yeah! The War on Terror was great!")
-else:
-    print("You must type A or B, let's just say the 1910s is your favorite decade.")
-    decade = "A"
-
-# ask the candidate a fifth question
-travel = input("What's your favorite way to travel?\n(A) Driving\n(B) Flying\n")
-if travel == "A":
-    print("Driving? I hope you like cars!")
-elif travel == "B":
-    print("Flying? I hope you like airplanes!")
-else:
-    print("You must type A or B, let's just say driving is your favorite way to travel.")
-    travel = "A"
+# ask the candidate each question of the quiz
+for index, question in enumerate(questions):
+    answer = input(question)
+    if answer == "A":
+        print(response_A[index])
+    elif answer == "B":
+        print(response_B[index])
+    else:
+        print("You must type A or B, let's just say you chose A.\n")
+        answer = "A"
+    answers += answer
 
 # print out their choices
-print(f"You chose {activity}, then {job}, then {value}, then {decade}, then {travel}.")
+print(f"You chose {answers[0]}, then {answers[1]}, then {answers[2]}, then {answers[3]}, then {answers[4]}.\n")
 
 # create some variables for scoring
 sam_like = 0
@@ -58,7 +41,7 @@ kai_like = 0
 indy_like = 0
 
 # update scoring variables based on the activity choice
-if activity == "A":
+if answers[0] == "A":
     sam_like = sam_like + 2
     indy_like = indy_like + 2
     kai_like = kai_like + 2
@@ -67,7 +50,7 @@ else:
     indy_like = indy_like + 1
 
 # update scoring variables based on the job choice
-if job == "A":
+if answers[1] == "A":
     sam_like = sam_like + 2
     indy_like = indy_like + 2
     cam_like = cam_like - 1
@@ -77,7 +60,7 @@ else:
     indy_like = indy_like + 1
 
 # update scoring variables based on the value choice
-if value == "A":
+if answers[2] == "A":
     sam_like = sam_like - 1
     kai_like = kai_like + 1
 else:
@@ -86,7 +69,7 @@ else:
     indy_like = indy_like + 1
 
 # update scoring variables based on the decade choice
-if decade == "A":
+if answers[3] == "A":
     cam_like = cam_like + 2
     sam_like = sam_like + 2
 else:
@@ -94,7 +77,7 @@ else:
     indy_like = indy_like + 2
 
 # update scoring variables based on the travel choice
-if travel == "A":
+if answers[4] == "A":
     sam_like = sam_like - 2
     kai_like = kai_like + 1
     indy_like = indy_like - 1
@@ -115,5 +98,5 @@ else:
 
 # More challenges for your quiz code
 # As an added challenge, you can start exploring how you might:
-#   Reduce the number of if, elif, and else statements in your code.
+#   Reduce the number of if, elif, and else statements in your code. -> DONE
 #   Allow users to select a new choice if they didn't enter A or B.
