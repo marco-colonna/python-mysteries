71d647231684566f1799f8a0340a5f4ff5c18ed3
marco-colonna <mcolonna107@gmail.com>
2022-07-07 17:52:26 -0400

    Challenge - Create a function to read in an entire message without having to print each word individually

--- decrypt.py
+++ decrypt.py
@@ -1,4 +1,4 @@
-# Define a function to find the truth by shifting the character by the specified amount
+# Define a function to shift the character by the specified amount
 def lasso_character(character, shift_amount):
     # Lowercase character (default)
     # Invoke the ord function to translate the character to its ASCII code
@@ -42,8 +42,7 @@ def lasso_character(character, shift_amount):
     # Send the decoded character back
     return decoded_character
 
-# Define a function to find the truth in a secret message
-# Shift the characters in a word by a specified amount to discover the hidden word
+# Define a function to shift the characters in a word by a specified amount
 def lasso_word(word, shift_amount):
     # This variable is updated each time another character is decoded
     decoded_word = ""
@@ -60,14 +59,32 @@ def lasso_word(word, shift_amount):
     # The decoded_word is sent back to the line of code that invoked this function
     return decoded_word
 
+# Define a function to find the truth in a secret message
+def lasso_message(message, shift_amount):
+    # This variable is updated each time another word is decoded
+    decoded_message = []
+
+    # This for loop iterates through each word in the message parameter
+    for index, word in enumerate(message):
+        # The lasso_word() function is invoked with each word and the shift amount
+        # The result (the decoded word) is stored in a variable called decoded_word
+        decoded_word = lasso_word(word, shift_amount[index])
+
+        # The decoded_word value is added to the end of the decoded_message value
+        decoded_message.append(decoded_word)
+
+    # The decoded_message is sent back to the line of code that invoked this function
+    return decoded_message
+
 # Try to decode the secret, encoded message
-print("Shifting Ncevy by 13 gives: \n" + lasso_word("Ncevy", 13))
-print("Shifting gpvsui by 25 gives: \n" + lasso_word("gpvsui", 25))
-print("Shifting ugflgkg by -18 gives: \n" + lasso_word("ugflgkg", -18))
-print("Shifting wjmmf by -1 gives: \n" + lasso_word("wjmmf", -1))
+words = ["Ncevy", "gpvsui", "ugflgkg", "wjmmf"]
+
+shifts = [13, 25, -18, -1]
+
+print("Shifting ", words, " by ", shifts, " gives: \n", lasso_message(words, shifts), sep = "")
 
 # Additional challenges for your decrypt code
 # As an added challenge, you can explore how to:
 #   Maintain casing for each letter throughout the decoding process. -> DONE
-#   Create a function to read in an entire message without having to print each word individually.
+#   Create a function to read in an entire message without having to print each word individually. -> DONE
 #   Modify your decoder to include numbers. -> DONE
